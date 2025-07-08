70d599715c400827d0f46c812d057940a3edc2b3
Mohamed00Abdelmonem <mmohamedabdelm@gmail.com>
2025-07-08 10:41:23 +0300

    make nice perfomance

---
+++ cleanup.bat
@@ -0,0 +1,7 @@
+@echo off
+rmdir /s /q "D:\Templates Frontend\version 1\portfolio-fainaly\owl.carousel"
+rmdir /s /q "D:\Templates Frontend\version 1\portfolio-fainaly\magnific-popup"
+rmdir /s /q "D:\Templates Frontend\version 1\portfolio-fainaly\jquery.countTo"
+rmdir /s /q "D:\Templates Frontend\version 1\portfolio-fainaly\scss"
+rmdir /s /q "D:\Templates Frontend\version 1\portfolio-fainaly\node_modules"
+
